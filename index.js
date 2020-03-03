e468d8015d04c088c3c254307fa568c67808c0b1
iamkael <michelmbili@gmail.com>
2020-03-03 10:06:11 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env/ node
+
+// NPM packages require
+const chalk = require("chalk")
+const clear = require("clear")
+const figlet = require("figlet")
+
+// Files require
+const files = require("./lib/questions")
+const inquirer = require("./lib/inquirer")
+
+// Start the main functions of the app
+clear()
+console.log(
+	chalk.blue(
+		figlet.textSync("WPTB", {
+			font: "Standard",
+			horizontalLayout: "default",
+			kerning: "fitted"
+		})
+	)
+)
+
+inquirer.wptbQuestions()
